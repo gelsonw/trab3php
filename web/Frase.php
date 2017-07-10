05d43738bcad12df19576e27ab9ffdfccbd16438
Gelson.Gonçalves <gelsonw@gmail.com>
2017-07-09 23:40:32 -0300

    Ajustes no index e testes

--- web/Frase.php
+++ web/Frase.php
@@ -13,3 +13,4 @@ class Frase  {
 	{
 		return $this->frase;
 	}
+}
--- web/index.php
+++ web/index.php
@@ -1,3 +1,40 @@
-<?php
 
-echo "Hello World!";
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Trabalho 3 - Metodologias Ágeis</title> 
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   </head>
+   <body>
+   <form method='POST'>
+   <h1>Trabalho 3 - Metodologias Ágeis</h1>
+   <p>
+   Professor: Daniel Wildt<br>
+   Aluno....: Gelson W. Gonçalves
+   </p>
+   <h3>Digite uma frase:</h3>
+ <input type="text" id="frase" name="frase">
+ <input type="submit" value="Enviar">
+ </form>
+<?php
+require_once 'Frase.php';
+
+$frase = new Frase();
+$frase->setFrase('');
+
+
+if (isset($_POST['frase'])) { 
+	$frase->setFrase($_POST['frase']);
+} else {
+	$frase->setFrase('Metodologias ÁGeis');
+}	
+
+if(!empty($frase->getFrase())){
+	echo "<h3><MARQUEE HEIGHT=30 WIDTH=640 BEHAVIOR=ALTERNATE SCROLLAMOUNT=10>".$frase->getFrase()." </MARQUEE>";
+} else {
+  	echo "Digite uma frase por favor";
+} 
+?>
+
+</body>
+</html>
